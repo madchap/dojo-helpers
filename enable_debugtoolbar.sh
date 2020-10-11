40b1afde5924531243f9580d0429842e2777b463
Fred Blaise <fred.blaise@protonmail.com>
2020-10-11 08:47:46 +0200

    fixes to enable debug_toolbar

--- enable_debugtoolbar.sh
+++ enable_debugtoolbar.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
-set -xe
+set -e
 
 settings_file=dojo/settings/settings.py
 
 [[ ! -f $settings_file ]] &&
     echo "File settings.py not found. Make sure to execute this from within your defectdojo root directory. Aborting." && exit 1
 
-if ! grep -qE 'debug_toolbar', $settings_file ; then
-    sed -i '/INSTALLED_APPS = (/a \    ''debug_toolbar'','  $settings_file
-    sed -i '/DJANGO_MIDDLEWARE_CLASSES = \[/a \    ''debug_toolbar.middleware.DebugToolbarMiddleware'','  $settings_file
+if ! grep -q 'import debug_toolbar' $settings_file ; then
+    # settings file
+    sed -i '/import os/i import debug_toolbar' $settings_file
+    sed -i '/INSTALLED_APPS = (/a \    '\'debug_toolbar\'','  $settings_file
+    sed -i '/DJANGO_MIDDLEWARE_CLASSES = \[/a \    '\'debug_toolbar.middleware.DebugToolbarMiddleware\'','  $settings_file
 
     TOOLFORCE="
 ## force toolbar to show no matter what
@@ -21,6 +23,17 @@ if ! grep -qE 'debug_toolbar', $settings_file ; then
 
     echo "INTERNAL_IPS = type(str('c'), (), {'__contains__': lambda *a: True})()" >> $settings_file
     echo "$TOOLFORCE" >> $settings_file
+
+    # requirements.txt
+    echo "django-debug-toolbar" >> requirements.txt
+
+    # urls.py
+    sed -i '/if settings.DEBUG:/a \    import debug_toolbar \
+    urlpatterns.insert(0, url(r"^__debug__/", include(debug_toolbar.urls)))' dojo/urls.py
+    
+    echo "Django debug toolbar enabled."
+    echo "Please rebuild your docker images to enable the debug toolbar library."
+
 else
     echo "Debug toolbar seems to be already enabled."
 fi
